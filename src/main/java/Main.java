524beab56e8d1645715a4a4fd7f380d66ba9a661
NicholasKir <92822987+NicholasKir@users.noreply.github.com>
2021-12-22 01:14:19 +0300

    CSV 1.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,9 @@ public class Main {
         Scanner console = new Scanner(System.in);
         String path = console.nextLine();
         String separator = ";"; //444
-        List <Person> companyList = new LinkedList<>();
-        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(path)){
+        int size = 0;
+        List<Person> companyList = new LinkedList<>();
+        try (InputStream in = Main.class.getClassLoader().getResourceAsStream(path)) {
 
             CSVReaderBuilder csvReaderBuilder = new CSVReaderBuilder(new FileReader(path));
             CSVReader csvReader = csvReaderBuilder.withSkipLines(1).build();
@@ -25,46 +26,54 @@ public class Main {
             if (csvReader == null)
                 throw new FileNotFoundException(path);
             String[] nextLine;
-            while ((nextLine = csvReader.readNext()) != null){
+            while ((nextLine = csvReader.readNext()) != null) {
                 nextLine = nextLine[0].split(separator);
-                if(nextLine.length > 2){
+                if (nextLine.length > 2) {
                     Person person = new Person(nextLine[0], nextLine[1], nextLine[2], nextLine[3], nextLine[4].charAt(0) - 'A', nextLine[4], nextLine[5]);
                     companyList.add(person);
                 }
             }
 
             System.out.println("Input the count of values : ");
-            int size = console.nextInt();
-            System.out.print("При вводе неправильного значения, например, отрицательного, ");
-            System.out.println("будут выведены все элементы списка.");
-
-            if(size > 0) {
-                System.out.println("Id\t\tName\tGender  Division Birthday   Salary");
-                for (int i = 0; i < companyList.size() && i < size; i++)
-                {
-                    System.out.print(companyList.get(i).getID()+"\t");
-                    System.out.print(companyList.get(i).getName()+"\t");
-                    System.out.print(companyList.get(i).getGender()+"\t");
-                    System.out.print(companyList.get(i).getBirthDate()+"\t");
-                    System.out.print(companyList.get(i).getSubdivisionID()+"\t");
-                    System.out.print(companyList.get(i).getSubdivisionName()+"\t");
-                    System.out.print(companyList.get(i).getSalary()+"\t");
-                    System.out.print("\n");
+            while (true) {
+                String k = console.next();
+                if (isDigit(k)) {
+                    if (Integer.parseInt(k) <= 0) System.out.println("Incorrect input!");
+                    else {
+                        size = Integer.parseInt(k);
+                        break;
+                    }
+                } else System.out.println("Incorrect type!");
+                while (!console.hasNextInt() || console.nextInt() <= 0) {
+                    System.out.println("Incorrect type!");
+                    console.next();
                 }
-
-                System.out.println("\nВывод при помощи toString(): ");
-                for (int i = 0; i < companyList.size() && i < size; i++) {
-                    System.out.println(companyList.get(i).toString());
+                if (console.hasNextInt() && console.nextInt() > 0) {
+                    System.out.println("Id\t\tName\tGender  Division Birthday   Salary");
+                    for (int i = 0; i < companyList.size() && i < size; i++) {
+                        System.out.print(companyList.get(i).getID() + "\t");
+                        System.out.print(companyList.get(i).getName() + "\t");
+                        System.out.print(companyList.get(i).getGender() + "\t");
+                        System.out.print(companyList.get(i).getBirthDate() + "\t");
+                        System.out.print(companyList.get(i).getSubdivisionID() + "\t");
+                        System.out.print(companyList.get(i).getSubdivisionName() + "\t");
+                        System.out.print(companyList.get(i).getSalary() + "\t");
+                        System.out.print("\n");
+                    }
+                    System.out.println("\nВывод при помощи toString(): ");
+                    for (int i = 0; i < companyList.size() && i < size; i++) {
+                        System.out.println(companyList.get(i).toString());
+                    }
                 }
             }
-            else {
-                System.out.println("Incorrect type!");
+        }
+        private static boolean isDigit (String s) throws NumberFormatException {
+            try {
+                Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
             }
         }
-        catch(FileNotFoundException except) {
-            System.out.println("Incorrect way to file!");
-        }
-
-
     }
 }
